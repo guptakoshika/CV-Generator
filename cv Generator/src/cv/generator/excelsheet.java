8d10fed83de98d3b499fa88f5a2634fe26e9ed57
Koshika Gupta <koshikaje@gmail.com>
2018-07-11 13:01:06 +0530

    Update excelsheet.java

--- cv Generator/src/cv/generator/excelsheet.java
+++ cv Generator/src/cv/generator/excelsheet.java
@@ -1,51 +1,50 @@
 package cv.generator;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.*;
+import java.util.List;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
 public class excelsheet {
-    void savetoexcel(cv o) throws IOException
-    {
-           XSSFWorkbook workbook = new XSSFWorkbook(); 
-           XSSFSheet spreadsheet = workbook.createSheet("CV DATA");
 
-            XSSFRow row;
-
-      //This data needs to be written (Object[])
-      Map < String, Object[] > empinfo = 
-      new TreeMap < String, Object[] >();
-      //empinfo.put( "1", new Object[] { "FIRST NAME", "LAST NAME", "CONTACT NUMBER","EMAIL","DATE OF BIRTH" });
-      empinfo.put( "1", new Object[] { o.getFname(),o.getSname(),o.getContact(),o.getEmail(),o.getDob()});
-      
-      //Iterate over data and write to sheet
-      Set < String > keyid = empinfo.keySet();
-      int rowid = 0;
-
-      for (String key : keyid) {
-         row = spreadsheet.createRow(rowid++);
-         Object [] objectArr = empinfo.get(key);
-         int cellid = 0;
-
-         for (Object obj : objectArr) {
-            Cell cell = row.createCell(cellid++);
-            cell.setCellValue((String)obj);
-         }
-      }
-
-      //Write the workbook in file system
-      FileOutputStream out = new FileOutputStream(new File("C:\\Users\\Koshika Gupta\\Desktop\\java\\Writesheet.xlsx"));
-      workbook.write(out);
-      out.close();
-      System.out.println("Writesheet.xlsx written successfully");
+    int createexcel(String excelpath) throws FileNotFoundException, IOException {
+        
+    String filename = excelpath+java.time.LocalDate.now()+".xls";
+    HSSFWorkbook workbook = new HSSFWorkbook();
+    HSSFSheet sheet = workbook.createSheet("firstsheet");
+    HSSFRow rowhead = sheet.createRow((short)0);
+    rowhead.createCell(0).setCellValue("Name");
+    rowhead.createCell(1).setCellValue("DOB");
+    rowhead.createCell(2).setCellValue("Address1");
+    rowhead.createCell(3).setCellValue("Address2");
+    rowhead.createCell(4).setCellValue("contact number");
+    rowhead.createCell(5).setCellValue("Email");
+    
+    Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory s = cfg.buildSessionFactory();
+        Session session = s.openSession();
+        Transaction tx = session.beginTransaction();
+        
+        int i =1;
+        Query q = session.createQuery("from cv");
+        List <cv> data = q.list();
+         for (cv obj : data)
+        {
+            HSSFRow row = sheet.createRow((short)i);
+            row.createCell(0).setCellValue(obj.getFname()+obj.getSname());
+            row.createCell(1).setCellValue(obj.getDob());
+            row.createCell(2).setCellValue(obj.getAdress1());
+            row.createCell(3).setCellValue(obj.getAddress2());
+            row.createCell(4).setCellValue(obj.getContact());
+            row.createCell(5).setCellValue(obj.getEmail());
+            i++;
+        }
+         FileOutputStream fileOut = new FileOutputStream(filename);
+         workbook.write(fileOut);
+         fileOut.close();
+         return 1;
     }
 }
