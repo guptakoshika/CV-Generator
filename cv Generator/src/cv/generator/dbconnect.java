1307bea215c2e6242eed4d7e3e3d8d9a95220aac
Koshika Gupta <koshikaje@gmail.com>
2018-07-13 10:07:26 +0530

    Update dbconnect.java

--- cv Generator/src/cv/generator/dbconnect.java
+++ cv Generator/src/cv/generator/dbconnect.java
@@ -1,16 +1,15 @@
-
 package cv.generator;
 
+import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-public class dbconnect{
-    
-    void savedata(cv c)
-    {
+public class dbconnect {
+
+    void savedata(cv c) {
         Configuration cfg = new Configuration();
         cfg.configure();
         SessionFactory sessionfac = cfg.buildSessionFactory();
@@ -21,17 +20,50 @@ public class dbconnect{
         s.close();
         sessionfac.close();
     }
-    cv fetch(int n){
-         Configuration cfg = new Configuration();
+
+    cv fetch(int n) {
+        Configuration cfg = new Configuration();
         cfg.configure();
         SessionFactory s = cfg.buildSessionFactory();
-        Session session = s.openSession();  
-      
+        Session session = s.openSession();
+
         Transaction tx = session.beginTransaction();
         Query q = session.createQuery("from cv");
-        Object k = session.get(cv.class,n);
+        Object k = session.get(cv.class, n);
         cv stud = (cv) k;
-         tx.commit();
-        return stud;  
-    }    
- }
+        tx.commit();
+        return stud;
+    }
+
+    void saveuserinfo(userdata u) {
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory sessionfac = cfg.buildSessionFactory();
+        Session s = sessionfac.openSession();
+        Transaction t = s.beginTransaction();
+        s.save(u);
+        t.commit();
+        s.close();
+        sessionfac.close();
+    }
+
+    int fetchdata(String email, String password) {
+        System.out.println(email);
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory sessionfac = cfg.buildSessionFactory();
+        Session s = sessionfac.openSession();
+        Transaction t = s.beginTransaction();
+        Query q = s.createQuery("from userdata");
+        Object k = s.createCriteria(email);
+        List<userdata> list = q.list();
+
+        for (userdata data : list) {
+            if (data.getEmailid().equals(email) && data.getPassword().equals(password) ) {
+                System.out.println(data.getEmailid());
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
