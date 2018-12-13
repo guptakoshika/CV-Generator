aa57b5670d3423abdd730c9213c01a908a1113dc
Koshika Gupta <koshikaje@gmail.com>
2018-12-13 09:38:24 +0530

    Update Validations.java

--- cv Generator/src/cv/generator/Validations.java
+++ cv Generator/src/cv/generator/Validations.java
@@ -6,19 +6,27 @@ import java.util.regex.Pattern;
 
 public class Validations {
      String validateData(cv obj){
+          //validate first, last name and nationality
          String nameregx = "^[\\p{L} .'-]+$";
-         Pattern pattern = Pattern.compile(nameregx,Pattern.CASE_INSENSITIVE);
-         Matcher matcher = pattern.matcher(obj.getFname());
-       if(matcher.find() == false)
+         Pattern namePattern = Pattern.compile(nameregx,Pattern.CASE_INSENSITIVE);
+         Matcher fmatcher = namePattern.matcher(obj.getFname());
+         Matcher lmatcher = namePattern.matcher(obj.getSname());
+         Matcher nmatcher = namePattern.matcher(obj.getNationality());
+       if(fmatcher.find() == false || lmatcher.find() == false )
            return "name is not valid please check and retry again!";
+       else if(nmatcher.find()== false )
+             return "nationality is not valid! please check and try again later!";
          
+       //phone number validation
+       
+       //validate email
          String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
                             "[a-zA-Z0-9_+&*-]+)*@" + 
                             "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
                             "A-Z]{2,7}$"; 
                               
-        Pattern pat = Pattern.compile(emailRegex); 
-        if (pat.matcher(obj.getEmail()).matches() == true)
+        Pattern emailpat = Pattern.compile(emailRegex); 
+        if (emailpat.matcher(obj.getEmail()).matches() == true)
              return "okay";
         return "email is not valid! Please check and retry again!";
      }    
